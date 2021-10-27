e0c48150db3457b315b8ce7463fbb7827ea6e8c0
Di Wu <xiaowu200031@gmail.com>
2021-10-27 15:24:44 -0700

    login skeleton redo

--- vacatary/app.js
+++ vacatary/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require("./routes/login");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -24,8 +25,9 @@ client.connect('mongodb://localhost:27017/', (err) => {
 	app.use(express.static(path.join(__dirname, 'public')));
 
 	app.use('/', indexRouter);
-	app.use('/users', usersRouter);
+	// app.use('/users', usersRouter);
 	app.use('/login', loginRouter);
+	app.use('/api', apiRouter);
 
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
--- vacatary/routes/login.js
+++ vacatary/routes/login.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET login page. */
+// /* GET login page. */
+// router.get('/', function(req, res, next) {
+//     res.render('login', { title: 'Vacatary Login' });
+// });
+
+
+/* GET login. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Vacatary Login' });
+	let has_redirect = (req.query.redirect != null);
+	let prompt = "Please Sign In";
+  	res.render('login', { has_redirect: has_redirect, redirect_target: req.query.redirect, prompt: prompt });
 });
+
 
 module.exports = router;
