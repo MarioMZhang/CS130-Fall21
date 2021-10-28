418ffc0cd3c2fc5d0599cdcdd247103c11136a57
andrewqu2000 <49246422+andrewqu2000@users.noreply.github.com>
2021-10-27 17:44:25 -0700

    Location format

--- tests/backend/db.sh
+++ tests/backend/db.sh
@@ -4,6 +4,6 @@ db.createCollection("Users")
 db.createCollection("Hubs")
 db.createCollection("Jobs")
 
-db.Hubs.insertMany([{ "hubId": 1: "description": "South entry of Bruin Plaza", "location": "34.0689, -118.4452", "startTime": 1635313479, "endTime": 1635314479}])
+db.Hubs.insertMany([{ "hubId": 1: "description": "South entry of Bruin Plaza", "location": {"lat": "34.0689", "long": "-118.4452"}, "startTime": 1635313479, "endTime": 1635314479}])
 db.Users.insertMany([{ "type": 1, "name": "Stan Marsh", "username": "smarsh", "password": "$2a$10$2DGJ96C77f/WwIwClPwSNuQRqjoSnDFj9GDKjg6X/PePgFdXoE4W6", "email": smarsh@spelementary.edu, "phone": "7190000000" },{ "type": 2, "name": "Kyle Broflovski", "username": "kyle_brof_driver", "password": "$2a$10$2DGJ96C77f/WwIwClPwSNuQRqjoSnDFj9GDKjg6X/PePgFdXoE4W6", "email": kyle@spelementary.edu, "phone": "7190000001" }])
 db.Jobs.insertMany([{"type": 1, "scheduledTime": 1635313499, "status": 0, "licenceState": "CO", "licenceNum": "CABD12", "hubId": 1, "code": 132561, "carLocation": null, "note": null, "driverUsername": null, "customerUsername": "smarsh"}])
