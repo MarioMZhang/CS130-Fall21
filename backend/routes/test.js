ce4317e745c628e84183d0439456fdf0546c035b
EricLiu <jiaheliucs@gmail.com>
2021-11-10 20:33:25 -0800

    Database text file improved

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -9,6 +9,12 @@ let unassignedJob = {"type": 1, "jobId": 3, "scheduledTime": 1635313500, "status
 let kmFile = {"type": 2, "username": 'princess_kenny', "name": 'Kenny McCormick', "password": '$2a$10$2DGJ96C77f/WwIwClPwSNuQRqjoSnDFj9GDKjg6X/PePgFdXoE4W6', "email": 'kenny@spelementary.edu', "phone": "7190000003", "driverStatus": 1};
 let jobToAssign = {"type": 1, "jobId": 4, "scheduledTime": 1635313500, "status": 1, "licenceState": "CO", "licenceNum": "CABD12", "hubId": 1, "code": 132561, "carLocation": null, "note": null, "driverUsername": null, "customerUsername": "ecartman", "advanceState": [0, 0]}
 
+
+let activhub3 = { "hubId": 3, "description": "Archaida", "location": [-123.23, 56.0690], "startTime": 1636095480, "endTime": 1736095091}
+
+let nonactivhub3 = { "hubId": 4, "description": "Archaida", "location": [-123.23, 56.0690], "startTime": 1036095480, "endTime": 1136095091}
+let newJob2 = {"type": 1, "jobId": 4, "scheduledTime": 1735313500, "status": 2, "licenceState": "CA", "licenceNum": "XXXX", "hubId": 1, "code": 555332, "carLocation": null, "note": null, "driverUsername": 'truck_driver', "customerUsername": "Justin", "advanceState": [0, 0]}
+
 /* Hubs api. */
 /* GET api. */
 router.get('/post1', function(req, res, next) {
@@ -32,14 +38,17 @@ router.get('/get1', function(req, res, next) {
     })
 })
 
-router.get('/get2', function(req, res, next) {
-	res.status(200);
-    grandValet.Database.read_activeHubs()
-    .then((fileFromDB) => {
-        res.send(fileFromDB);
-    })
-})
 
+//Store nearby hubs in Hubs DB
+router.get('/post2', function(req,res,next){
+    res.status(200);
+    grandValet.Database.store_hub(nearby);
+    grandValet.Database.store_hub(activhub3);
+    grandValet.Database.store_hub(nonactivhub3);
+    res.send("GET /api/hubs placeholder");
+} )
+
+//Test nearby_hub
 router.get('/get3', function(req, res, next) {
 	res.status(200);
     grandValet.Database.read_nearbyHubs([-118.4452, 34.0689], 0.5)
@@ -48,9 +57,19 @@ router.get('/get3', function(req, res, next) {
     })
 })
 
+
+router.get('/get2', function(req, res, next) {
+	res.status(200);
+    grandValet.Database.read_activeHubs()
+    .then((fileFromDB) => {
+        res.send(fileFromDB);
+    })
+})
+
+
 router.get('/get4', function(req, res, next) {
 	res.status(200);
-    grandValet.Database.read_hub(1)
+    grandValet.Database.read_hub(2)
     .then((fileFromDB) => {
         res.send(fileFromDB);
     })
@@ -60,12 +79,13 @@ router.get('/post3', function(req, res, next) {
 	res.status(200);
     //let gv = grandValet.Database.db('GrandValet');
     grandValet.Database.store_job(newJob);
+    grandValet.Database.store_job(newJob2);
 	res.send("GET /api/hubs placeholder");
 })
 
 router.get('/get5', function(req, res, next) {
 	res.status(200);
-    grandValet.Database.read_job(2)
+    grandValet.Database.read_job(4)
     .then((fileFromDB) => {
         res.send(fileFromDB);
     })
@@ -73,7 +93,7 @@ router.get('/get5', function(req, res, next) {
 
 router.get('/get6', function(req, res, next) {
 	res.status(200);
-    grandValet.Database.read_assignedJobs("kyle_brof_driver")
+    grandValet.Database.read_assignedJobs("truck_driver")
     .then((fileFromDB) => {
         res.send(fileFromDB);
     })
