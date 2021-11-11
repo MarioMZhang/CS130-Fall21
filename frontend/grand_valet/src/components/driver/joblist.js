73a7cf08d19c7c99292ebf3c79a21d816b9bb1b5
Zhang <MZHANG0624@gmail.com>
2021-11-10 17:18:55 -0800

    change

--- frontend/grand_valet/src/components/driver/joblist.js
+++ frontend/grand_valet/src/components/driver/joblist.js
@@ -3,6 +3,12 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from "@mui/material/Grid";
 import CssBaseline from "@mui/material/CssBaseline";
 import {reactFormatter, ReactTabulator} from "react-tabulator";
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function SimpleButton(props) {
     const rowData = props.cell._cell.row.data;
@@ -31,6 +37,7 @@ const table_columns = [
                 cell.getRow().delete();
             }
         }}
+
 ];
 
 var table_data = [
@@ -71,7 +78,65 @@ export default class Joblist extends React.Component{
                             />
                         </div>
                     </Grid>
-
+                    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                        <Box
+                            sx={{
+                                my: 8,
+                                mx: 4,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Typography component="h1" variant="h5">
+                                Schedule Dropoff
+                            </Typography>
+                            <Box component="form" noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                                <br/>
+                                <br/>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="#" variant="body2">
+                                            Forgot password?
+                                        </Link>
+                                    </Grid>
+                                    <Grid item>
+                                        <Link href="/signup" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Grid>
                 </Grid>
             </ThemeProvider>
         );
