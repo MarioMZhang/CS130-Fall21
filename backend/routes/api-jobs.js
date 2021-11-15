71ec5f50c65d8700cfb391c516111591bccb00d1
Di Wu <xiaowu200031@gmail.com>
2021-11-14 16:16:09 -0800

    update

--- backend/routes/api-jobs.js
+++ backend/routes/api-jobs.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-// const server=express();
-// const bodyParser = require('body-parser');
-// server.use(bodyParser.urlencoded());
-var createError = require('http-errors');
 
+let GrandValet = require('../db-v2');
 
-let dbv2 = require('../db-v2');
 
 /* Jobs api. */
 /* GET api. */
@@ -24,7 +20,7 @@ router.get('/jobs',function(req, res, next) {
 
     if(jobid != null){
         jobid = parseInt(jobid);
-        dbv2.Database.read_job(jobid).then((result)=>{
+        GrandValet.Database.read_job(jobid).then((result)=>{
         if(result != null){
             res.status(200);
             res.json(result);
@@ -37,7 +33,7 @@ router.get('/jobs',function(req, res, next) {
     }
 
     if(username != null){
-        dbv2.Database.read_assignedJobs(username).then((result)=>{
+        GrandValet.Database.read_assignedJobs(username).then((result)=>{
         if(result != null){
             res.json(result);
         }
@@ -46,26 +42,266 @@ router.get('/jobs',function(req, res, next) {
             res.send("Job not found for driver " + username);
         }
     });
-  }
-
-    
+  } 
 });
 
 
 /* POST api. */
 router.post('/jobs',function(req, res, next) {
+    // input checks 
     console.log(req.body);
-    // console.log(req.body.jobid>0);
-    // dbv2.Database.store_job(req.body).then((result)=>{
-    //    if(result!=""){
-    //        res.status(201);
-    //        res.json(result);
-    //    }else{
-    //        next(createError(404));
-    //    }
-    // });
-    res.status(201);
-    res.send("Job created");
+
+
+    if(req.body.jobId == null || req.body.type == null || req.body.status == null ) {
+        res.status(400);
+        res.send("Bad request format");
+        return;
+    }    
+
+    let jobid = parseInt(req.body.jobId);
+    let jobtype = parseInt(req.body.type);
+    let jobstatus = parseInt(req.body.status);
+    
+    if(jobtype < 1 || jobtype > 3 || jobstatus < 0 || jobstatus > 14) {
+        res.status(400);
+        res.send("Bad request format: jobtype/jobstatus");
+        return;
+    }
+
+    // For creating new jobs
+    if(jobid == 0) {
+        // TODO: for each customer/driver, maybe we need to set all previous 
+        // jobs to the finished state when we are inserting a new job.
+
+        // car dropoff
+        if(jobtype == 1) {
+            if(req.body.scheduledTime == null || req.body.licenceState == null 
+                 || req.body.licenceNum == null || req.body.hubId == null 
+                 || req.body.customerUsername == null) {
+                res.status(400);
+                res.send("Bad request format");
+                return;
+            }
+            if(jobstatus != 0 && jobstatus != 1) {
+                res.status(400);
+                res.send("jobstatus must be 0 or 1 for jobid=0 and jobtype=1");
+                return;
+            }
+
+            let jobScheduledTime = parseInt(req.body.scheduledTime);
+            let jobHubId = parseInt(req.body.hubId);
+
+            // check that user exists
+            GrandValet.Database.read_user(req.body.customerUsername).then((user) => {
+                if(user == null) {
+                        res.status(400);
+                        res.send("user does not exist");
+                        return;
+                }
+
+                // check that hub exists
+                GrandValet.Database.read_hub(jobHubId).then((hub) => {
+                    if(hub == null) {
+                        res.status(400);
+                        res.send("hub does not exist");
+                        return;
+                    }
+                    let time = Math.floor(Date.now() / 1000);
+                    if(time >= hub.endTime) {
+                        res.status(400);
+                        res.send("hub closed");
+                        return;
+                    }
+
+                    GrandValet.Database.read_maxMaxJobId().then((maxJobId) => {
+                        let new_entry = {};
+                        new_entry.jobId = maxJobId + 1;
+                        new_entry.type = jobtype;
+                        new_entry.status = 1;
+                        new_entry.scheduledTime = jobScheduledTime;
+                        new_entry.licenceState = req.body.licenceState;
+                        new_entry.licenceNum = req.body.licenceNum;
+                        new_entry.hubId = jobHubId;
+                        new_entry.code = null;
+                        new_entry.carLocation = null;
+                        new_entry.note = null;
+                        new_entry.driverUsername = null;
+                        new_entry.customerUsername = req.body.customerUsername;
+                        new_entry.advanceState = [0, 0];
+
+                        GrandValet.Database.store_job(new_entry).then(() => {
+                            // get again and return
+                            GrandValet.Database.read_job(new_entry.jobId).then((result)=>{
+                                if(result == null) {
+                                    res.status(400);
+                                    res.send("failed to create job");
+                                    return;
+                                }
+                                else {
+                                    res.status(201);
+                                    res.set('Content-Type', 'application/json');
+                                    res.json(result);
+                                    return;
+                                }
+                            })
+                        })
+                    })
+                })
+            })
+        }
+
+        // car pickup
+        else if(jobtype == 2) {
+            if(req.body.scheduledTime == null || req.body.licenceState == null 
+                 || req.body.licenceNum == null || req.body.hubId == null 
+                 || req.body.customerUsername == null) {
+                res.status(400);
+                res.send("Bad request format");
+                return;
+            }
+            if(jobstatus != 0 && jobstatus != 6) {
+                res.status(400);
+                res.send("jobstatus must be 0 or 6 for jobid=0 and jobtype=2");
+                return;
+            }
+
+            let jobScheduledTime = parseInt(req.body.scheduledTime);
+            let jobHubId = parseInt(req.body.hubId);
+
+            // check that user exists
+            GrandValet.Database.read_user(req.body.customerUsername).then((user) => {
+                if(user == null) {
+                        res.status(400);
+                        res.send("user does not exist");
+                        return;
+                }
+                // check that hub exists
+                GrandValet.Database.read_hub(jobHubId).then((hub) => {
+                    if(hub == null) {
+                        res.status(400);
+                        res.send("hub does not exist");
+                        return;
+                    }
+                    // TODO: maybe enforce the field "carLocation" and "note"
+
+                    // We allow creating pickup job after job closing time
+                    // let time = Math.floor(Date.now() / 1000);
+                    // if(time >= hub.endTime) {
+                    //     res.status(400);
+                    //     res.send("hub closed");
+                    //     return;
+                    // }
+
+                    GrandValet.Database.read_maxMaxJobId().then((maxJobId) => {
+                        let new_entry = {};
+                        new_entry.jobId = maxJobId + 1;
+                        new_entry.type = jobtype;
+                        new_entry.status = 6;
+                        new_entry.scheduledTime = jobScheduledTime;
+                        new_entry.licenceState = req.body.licenceState;
+                        new_entry.licenceNum = req.body.licenceNum;
+                        new_entry.hubId = jobHubId;
+                        new_entry.code = null;
+                        new_entry.carLocation = req.body.carLocation;
+                        new_entry.note = req.body.note;
+                        new_entry.driverUsername = null;
+                        new_entry.customerUsername = req.body.customerUsername;
+                        new_entry.advanceState = [0, 0];
+
+                        GrandValet.Database.store_job(new_entry).then(() => {
+                            // get again and return
+                            GrandValet.Database.read_job(new_entry.jobId).then((result)=>{
+                                if(result == null) {
+                                    res.status(400);
+                                    res.send("failed to create job");
+                                    return;
+                                }
+                                else {
+                                    res.status(201);
+                                    res.set('Content-Type', 'application/json');
+                                    res.json(result);
+                                    return;
+                                }
+                            })
+                        })
+                    })
+                })
+            })
+        }
+
+        // driver - break
+        else {
+            if(req.body.scheduledTime == null || req.body.driverUsername == null) {
+                res.status(400);
+                res.send("Bad request format");
+                return;
+            }
+            if(jobstatus != 0 && jobstatus != 11) {
+                res.status(400);
+                res.send("jobstatus must be 0 or 11 for jobid=0 and jobtype=3");
+                return;
+            }
+
+            let jobScheduledTime = parseInt(req.body.scheduledTime);
+            let time = Math.floor(Date.now() / 1000);
+            if(jobScheduledTime <= time) {
+                res.status(400);
+                res.send("schedule break time is in the past");
+                return;
+            }
+
+            // check that user exists
+            GrandValet.Database.read_user(req.body.driverUsername).then((user) => {
+                if(user == null) {
+                        res.status(400);
+                        res.send("user does not exist");
+                        return;
+                }
+
+                GrandValet.Database.read_maxMaxJobId().then((maxJobId) => {
+                    let new_entry = {};
+                    new_entry.jobId = maxJobId + 1;
+                    new_entry.type = jobtype;
+                    new_entry.status = 11;
+                    new_entry.scheduledTime = jobScheduledTime;
+                    new_entry.licenceState = null; 
+                    new_entry.licenceNum = null; 
+                    new_entry.hubId = null; 
+                    new_entry.code = null;
+                    new_entry.carLocation = null;
+                    new_entry.note = null;
+                    new_entry.driverUsername = req.body.driverUsername;
+                    new_entry.customerUsername = null;
+                    new_entry.advanceState = [0, 0];
+
+                    GrandValet.Database.store_job(new_entry).then(() => {
+                        // get again and return
+                        GrandValet.Database.read_job(new_entry.jobId).then((result)=>{
+                            if(result == null) {
+                                res.status(400);
+                                res.send("failed to create job");
+                                return;
+                            }
+                            else {
+                                res.status(201);
+                                res.set('Content-Type', 'application/json');
+                                res.json(result);
+                                return;
+                            }
+                        })
+                    })
+                })
+            })
+        }
+
+    } 
+
+    // TODO: updating existing jobs
+    else {
+        res.status(200);
+        res.send("To be implemented: updating existing jobs")
+
+    }
     
 });
 
--- tests/backend/test_jobs.py
+++ tests/backend/test_jobs.py
@@ -7,29 +7,40 @@ SERVER = "http://localhost:3000"
 
 class TestJobs(unittest.TestCase):
     # def test_jobs_getnotexist(self):
-    #     r = requests.get(SERVER + '/api/hubs?hubId=0')
-    #     self.assertEqual(r.status_code, 404)
-    #     r = requests.get(SERVER + '/api/hubs?hubId=-1')
-    #     self.assertEqual(r.status_code, 404)
-    #     r = requests.get(SERVER + '/api/hubs?hubId=alpha')
-    #     self.assertEqual(r.status_code, 404)
 
-    def test_hubs_post(self):
-        # new_hub = { 'hubId': 0, 'description': '330 De Neve Drive', 'location': [-118.9, 34.1], 'startTime': 1636095000, 'endTime': 1637095091 }
-        # r = requests.post(SERVER + '/api/hubs', data=new_hub)
-        # self.assertEqual(r.status_code, 201)
-        # hub = r.json()
-        # self.assertTrue(hub['hubId'] > 0)
-        # self.assertEqual(hub['description'], new_hub['description'])
-        # self.assertTrue(len(hub['location']) == 2 
-        #     and (hub['location'][0] == new_hub['location'][0])
-        #     and (hub['location'][1] == new_hub['location'][1]))
-
-        # self.assertEqual(hub['startTime'], new_hub['startTime'])
-        # self.assertEqual(hub['endTime'], new_hub['endTime'])
-        new_dropoff_job = {"type": 1, "jobId": 1, "scheduledTime": 1635313499, "status": 0, "licenceState": "CA", "licenceNum": "CA1234", "hubId": 1, "code": None, "carLocation": None, "note": None, "driverUsername": None, "customerUsername": "smarsh", "advanceState": [0, 0]}
+    def test_hubs_post_dropoff(self):
+        new_dropoff_job = {"type": 1, "jobId": 0, "scheduledTime": 1637313499, "status": 0, "licenceState": "CA", "licenceNum": "CA1234", "hubId": 1, "code": None, "carLocation": None, "note": None, "driverUsername": None, "customerUsername": "smarsh", "advanceState": [0, 0]}
         r = requests.post(SERVER + '/api/jobs', data=new_dropoff_job)
         self.assertEqual(r.status_code, 201)
+        print(r.json())
+
+        new_dropoff_job = {"type": 1, "jobId": 0, "scheduledTime": 1637313499, "status": 1, "licenceState": "CA", "licenceNum": "CA1234", "hubId": 1, "code": None, "carLocation": None, "note": None, "driverUsername": None, "customerUsername": "smarsh", "advanceState": [0, 0]}
+        r = requests.post(SERVER + '/api/jobs', data=new_dropoff_job)
+        self.assertEqual(r.status_code, 201)
+        print(r.json())
+
+    def test_hubs_post_pickup(self):
+        new_pickup_job = {"type": 2, "jobId": 0, "scheduledTime": 1637313499, "status": 0, "licenceState": "CA", "licenceNum": "CA1234", "hubId": 1, "code": None, "carLocation": None, "note": None, "driverUsername": None, "customerUsername": "smarsh", "advanceState": [0, 0]}
+        r = requests.post(SERVER + '/api/jobs', data=new_pickup_job)
+        self.assertEqual(r.status_code, 201)
+        print(r.json())
+
+        new_pickup_job = {"type": 2, "jobId": 0, "scheduledTime": 1637313499, "status": 6, "licenceState": "CA", "licenceNum": "CA1234", "hubId": 1, "code": None, "carLocation": None, "note": None, "driverUsername": None, "customerUsername": "smarsh", "advanceState": [0, 0]}
+        r = requests.post(SERVER + '/api/jobs', data=new_pickup_job)
+        self.assertEqual(r.status_code, 201)
+        print(r.json())
+
+    def test_hubs_post_break(self):
+        new_pickup_job = {"type": 3, "jobId": 0, "scheduledTime": 1637313499, "status": 11, "licenceState": None, "licenceNum": None, "hubId": None, "code": None, "carLocation": None, "note": None, "driverUsername": "kyle_brof_driver", "customerUsername": None, "advanceState": [0, 0]}
+        r = requests.post(SERVER + '/api/jobs', data=new_pickup_job)
+        self.assertEqual(r.status_code, 201)
+        print(r.json())
+
+        new_pickup_job = {"type": 3, "jobId": 0, "scheduledTime": 1637313499, "status": 0, "licenceState": None, "licenceNum": None, "hubId": None, "code": None, "carLocation": None, "note": None, "driverUsername": "kyle_brof_driver", "customerUsername": None, "advanceState": [0, 0]}
+        r = requests.post(SERVER + '/api/jobs', data=new_pickup_job)
+        self.assertEqual(r.status_code, 201)
+        print(r.json())
+
 
 if __name__ == '__main__':
     unittest.main()
